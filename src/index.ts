22387e47c06e32a3848520f5385f071e9633e237
ErranderLee <dre12am14@gmail.com>
2022-04-19 01:51:43 +0900

    Feat: Binding Socket.io to http Server

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
-import express from "express";
-class App {
-  public application : express.Application;
-  constructor(){
-    this.application = express();
-  }
-}
-const app = new App().application;
-app.get("/",(req : express.Request , res : express.Response) =>{
-  res.send("start!!!");
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const httpServer: any = createServer();
+const io: Server = new Server(httpServer, {
+    cors: {
+      origin: '*',
+    }
+});
+
+io.on('connection', (socket) => {
+  console.log('WebSocket Connected');
+  // 수신한 event에 대한 처리를 해주면 됩니다. 아래는 예시
+  socket.on('join', (params) => {
+      console.log(params.clientId, '가', params.roomNumber, '번 방에 입장합니다.');
+  });
+  socket.on('message', (params) => {
+      console.log(params.message);
+  });
 })
-app.listen(4000,()=>console.log("start"));
+
+httpServer.listen(4000, () => {
+  console.log('Server On');
+});
