be3d988e66df7a92cfa705d29a72e361788770d0
coreight98 <maxcha98@naver.com>
2022-04-23 02:35:34 +0900

    refactor : divide socket logic to socket file

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,14 @@
-import { createServer } from 'http';
-import { Server } from 'socket.io';
+import { createServer } from "http";
+import { Server } from "socket.io";
 
+import { getSocket } from "@socket/index";
 const httpServer: any = createServer();
 const io: Server = new Server(httpServer, {
-    cors: {
-      origin: '*',
-    }
+  cors: {
+    origin: "*",
+  },
 });
-
-io.on('connection', (socket) => {
-  console.log('WebSocket Connected');
-  // 수신한 event에 대한 처리를 해주면 됩니다. 아래는 예시
-  socket.on('join', (params) => {
-      console.log(params.clientId, '가', params.roomNumber, '번 방에 입장합니다.');
-  });
-  socket.on('message', (params) => {
-      console.log(params.message);
-  });
-})
-
+getSocket(io);
 httpServer.listen(4000, () => {
-  console.log('Server On');
+  console.log("Server On");
 });
